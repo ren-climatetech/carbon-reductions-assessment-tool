bcb0d20423432963559e366592de9980cac2580b
Ren <reine.donnestad@gmail.com>
2024-11-24 23:29:09 -0500

    homepage setup

--- carbon-reductions-assessment-tool-client/src/pages/HomePage/HomePage.jsx
+++ carbon-reductions-assessment-tool-client/src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,27 @@
-function HomePage() {}
+import React from "react";
+import "./HomePage.scss";
+import homepagecover from "../../assets/images/roadandtrees.jpg"
+
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      
+      <div className="homepage__section homepage__section-services">
+      <img className="homepage__image" src={homepagecover} alt="background-cvoer-photo" />
+      <div className="homepage__overlay"></div>
+        <div className="homepage__container">
+          <p className="homepage__title">Services</p>
+        </div>
+      </div>
+
+      <section>
+        
+      </section>
+      <section></section>
+      <footer></footer>
+    </div>
+  );
+}
 
 export default HomePage;
