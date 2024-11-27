492e958601dea37eec6d50a6a6ac33de45ad685a
Ren <reine.donnestad@gmail.com>
2024-11-27 14:19:07 -0500

    minor changes to results knex route

--- carbon-reductions-assessment-tool-api/migrations/20241127174237_create_results_table.js
+++ carbon-reductions-assessment-tool-api/migrations/20241127174237_create_results_table.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+export function up(knex) {
     return knex.schema.createTable("results", function (table) {
         table.string("refrigerationSystem").notNullable();
         table.string("coolantType").notNullable();
