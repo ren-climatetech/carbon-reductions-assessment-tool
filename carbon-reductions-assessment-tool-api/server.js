bea2de2e47031113ea2d54b2e8623921ededa950
Ren <reine.donnestad@gmail.com>
2024-11-26 22:15:38 -0500

    admin page setup

--- carbon-reductions-assessment-tool-api/server.js
+++ carbon-reductions-assessment-tool-api/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
-import systemsRoutes from "./routes/refrigSystems.js";
+import systemsRouter from "./routes/refrigSystems.js";
 
 const app = express() 
 
 app.use(cors())
 app.use(express.json());
-app.use("/api", systemsRoutes);
+app.use("/api", systemsRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
---
+++ carbon-reductions-assessment-tool-client/src/pages/AdminPage/AdminPage.js
@@ -0,0 +1,14 @@
+<>
+<ul className="Inputs">
+    <li>System Type</li>
+    <li>Coolant Type</li>
+    <li>Weight of Coolant (mT)</li> 
+</ul>
+
+<ul className="Outputs">
+    <li>Output A: mT CO2e of Refrigerant Type (mT * GWP) </li>
+    <li>Output B: mT CO2e (mT * System GWP)</li>
+    <li>Eligibility (Yes, if GWP of Coolant greater than GWP of system GWP)</li>
+    <li>Estimated Carbon Credits: If Elibility=Yes, ((Output A-OutputB) * 65)</li>
+</ul>
+</>
