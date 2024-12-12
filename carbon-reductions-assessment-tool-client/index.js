7ebc54e3731fec9dd45cf29f418124963fe371f1
Ren <reine.donnestad@gmail.com>
2024-12-12 15:48:59 -0500

    test

--- carbon-reductions-assessment-tool-client/index.js
+++ carbon-reductions-assessment-tool-client/index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-app.use(cors()); 
+app.use(cors());
 
 const app = express();
 
 const PORT = process.env.PORT || 5050;
 
-app.get('/', (req, res) => {
-    res.send(`<h1>Welcome to my Express App</h1>`);
+app.get("/", (req, res) => {
+  res.send(`<h1>Welcome to my Express App</h1>`);
 });
 
 app.listen(PORT, () => {
-   console.log(`app running on port ${PORT}`)
+  console.log(`app running on port ${PORT}`);
 });
