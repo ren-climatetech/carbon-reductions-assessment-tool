d1fbdceb2fe7b7ee85e8d97f5352efaf1b230932
Ren <reine.donnestad@gmail.com>
2024-12-13 11:26:02 -0500

    updated backend

--- carbon-reductions-assessment-tool-api/server.js
+++ carbon-reductions-assessment-tool-api/server.js
@@ -4,25 +4,17 @@ import systemsRouter from "./routes/refrigSystems.js";
 import coolantsRouter from "./routes/coolantData.js"; 
 import resultsRouter from "./routes/resultsRouter.js";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
-const { CORS_ORIGIN } = process.env;
-
 const app = express() 
 
-// app.use(cors())
-
-app.use(cors({ origin: CORS_ORIGIN }));
+app.use(cors());
 app.use(express.json());
 app.use("/api/systems", systemsRouter);
 app.use("/api/coolants", coolantsRouter);
 app.use("/api/results", resultsRouter); 
 
-
-
-
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
-  console.log(`Server running on ${BASE_URL}:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
 
 
